b9d44887c018477e6503297fba5c08cd318237a9
astatine575 <astatine575@gmail.com>
2019-02-14 18:53:22 -0600

    Added main menu

--- Program.cs
+++ Program.cs
@@ -12,8 +12,27 @@ namespace KingsisleTest
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey(true);
+            Console.Clear();
+
+            Console.SetCursorPosition((Console.WindowWidth - "Astatine's Card Games".Length) / 2, Console.CursorTop);
+            Console.WriteLine("Astatine's Card Games");
+
+            Console.SetCursorPosition((Console.WindowWidth - "As many games as Riot Games!".Length) / 2, Console.CursorTop + 1);
+            Console.WriteLine("As many games as Riot Games!");
+
+            String menu =  "[Q]uit to desktop\n";
+
+            Console.Write("\n" + menu);
+
+
+            ConsoleKey keyPressed;
+            bool leavemenu = false;
+            do
+            {
+                keyPressed = Console.ReadKey(true).Key;
+                
+                leavemenu = leavemenu || keyPressed == ConsoleKey.Q;
+            } while (!leavemenu);
 
         }
     }
